e7fb0dc43c31cb2ea1fa7c87ef59dc38e02dda5e
Jeffrey Scarmazzi <jeff8977@esri.com>
2018-10-30 20:48:13 -0400

    Let's Use Dates

--- routes/news.js
+++ routes/news.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var dateformat = require('dateformat');
 
 // PG Connection Values
 require('dotenv').config();
@@ -9,7 +10,8 @@ const client = new Client();
 client.connect();
 
 const the_sql = `
-                select distinct sourceurl, goldsteinscale::float, numarticles::integer from geom_20181027
+                select distinct sourceurl, goldsteinscale::float, numarticles::integer 
+                from geom_${dateformat(Date.now(), 'yyyymmdd')}
                 where left(eventcode, 2) in ('14') and numarticles::integer >= 25
                 order by goldsteinscale::float asc, numarticles::integer desc 
                 limit 5;
