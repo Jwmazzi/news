b6c56de9b94ce023ca7974b49c258c1e36b9315b
Jeffrey Scarmazzi <jeff8977@esri.com>
2018-11-01 19:40:26 -0400

    SQL File

---
+++ public/javascripts/sql.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+    latest:`
+           select tablename from pg_catalog.pg_tables 
+           where left(tablename, 4) = 'geom'
+           order by tablename desc limit 1
+           `
+            ,
+
+    protest:`
+            select distinct sourceurl, goldsteinscale::float, numarticles::integer from $1
+            where left(eventcode, 2) in ('14') and numarticles::integer >= 25
+            order by goldsteinscale::float asc, numarticles::integer desc 
+            limit 5;
+            `
+
+}
--- routes/news.js
+++ routes/news.js
@@ -1,39 +1,31 @@
 var express    = require('express');
 var router     = express.Router();
 var utils      = require('../public/javascripts/utils');
-
-var sql_a = `
-            select tablename from pg_catalog.pg_tables 
-            where left(tablename, 4) = 'geom'
-            order by tablename desc limit 1
-            `
-
-var sql_b = `
-            select distinct sourceurl, goldsteinscale::float, numarticles::integer from $1
-            where left(eventcode, 2) in ('14') and numarticles::integer >= 25
-            order by goldsteinscale::float asc, numarticles::integer desc 
-            limit 5;
-            `
+var sql        = require('../public/javascripts/sql');
 
 router.get('/', (rte_req, rte_res, next) => {
 
-  utils.query(sql_a, null, function(err, res) {
+  utils.query(sql.latest, null, function(err, res) {
 
     if (err) {
-
-      rte_res.render('error', {title: 'SQL Query Failed', error: err.stack});
       
+      rte_res.render('error', {title: 'SQL Query Failed', error: err.stack});
+
     } else {
 
       var the_tbl = res.rows[0].tablename;
-      var the_sql = sql_b.replace('$1', the_tbl)
+      var the_sql = sql.protest.replace('$1', the_tbl)
 
       utils.query(the_sql, null, function(err, res) {
 
-        if (err) { 
+        if (err) {
+
           rte_res.render('error', {title: 'SQL Query Failed', error: err.stack});
+
         } else {
+
           rte_res.render('news', {title: 'GDELT News', stories: res.rows});
+
         }
 
       });
